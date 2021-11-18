2c890102a9b27f725ceb6605b12a78becfa5ac3e
vicky <vigneshwaran2747@gmail.com>
2021-11-18 19:31:09 +0530

    Intial setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'chat-in-session',
+    appId: '1:21569649812:web:4ce73bafc57ef97dedc881',
+    databaseURL: 'https://chat-in-session-default-rtdb.firebaseio.com',
+    storageBucket: 'chat-in-session.appspot.com',
+    apiKey: 'AIzaSyCMpvR1V-JYv3rDxCD5VgknzoWxvUglAAU',
+    authDomain: 'chat-in-session.firebaseapp.com',
+    messagingSenderId: '21569649812',
+    measurementId: 'G-X73N218E5Q',
+  },
   production: true
 };
